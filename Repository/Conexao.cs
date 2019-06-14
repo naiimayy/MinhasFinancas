1378c4de2399493bac1c15218eb41f6d565ccfad
naiimayy <naiiaramay@gmail.com>
2019-06-14 15:17:26 -0300

    Criação da conexao no repositório

--- Repository/Conexao.cs
+++ Repository/Conexao.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Repository
 {
-    class Conexao
+    public class Conexao
     {
+        public SqlCommand Conectar()
+        {
+            SqlConnection conexao = new SqlConnection();
+            conexao.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\65979\Documents\MinhasFinancas.mdf;Integrated Security=True;Connect Timeout=30";
+            conexao.Open();
+
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexao;
+            return comando;
+        }
     }
 }
