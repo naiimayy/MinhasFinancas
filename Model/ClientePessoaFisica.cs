ebc8c9e934462e37cf7e216228bcc16297c60c8c
naiimayy <naiiaramay@gmail.com>
2019-06-19 16:46:51 -0300

    Desenvolvido Model Pessoa Física

---
+++ Model/ClientePessoaFisica.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class ClientePessoaFisica
+    {
+        public int Id;
+        public string Nome;
+        public string Cpf;
+        public DateTime Data_nascimento;
+        public string Rg;
+        public string Sexo;
+    }
+}
