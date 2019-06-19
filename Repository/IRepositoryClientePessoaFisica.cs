1de4fb7d50d298f4f6fe2f8ce6019e4b0820ceae
naiimayy <naiiaramay@gmail.com>
2019-06-19 16:47:57 -0300

    Criado IRepository Pessoa Fisica

---
+++ Repository/IRepositoryClientePessoaFisica.cs
@@ -0,0 +1,23 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    interface IRepositoryClientePessoaFisica
+    {
+        int Inserir(ClientePessoaFisica cliente);
+
+        List<ClientePessoaFisica> ObterTodos(string busca);
+
+        ClientePessoaFisica ObterPeloId(int id);
+
+        bool Atualizar(ClientePessoaFisica cliente);
+
+        bool Apagar(int id);
+    }
+}
+}
