61468253d6ceef8880783d3484e5bf19e8970d78
naiimayy <naiiaramay@gmail.com>
2019-06-19 15:35:10 -0300

    Desenvolvido conta a pagar por completo

--- Repository/ContaPagarRepositorio.cs
+++ Repository/ContaPagarRepositorio.cs
@@ -20,15 +20,15 @@ namespace Repository
 
         public int Inserir (ContaPagar conta)
         {
-            SqlCommand comando = new SqlCommand();
+            SqlCommand comando = conexao.Conectar();
             comando.CommandText = @"INSERT INTO contas_pagar (nome, valor, tipo, descricao, status)
-                                  VALUES @NOME, @VALOR, @TIPO, @DESCRICAO, @STATUS";
+                                  VALUES (@NOME, @VALOR, @TIPO, @DESCRICAO, @STATUS)";
 
             comando.Parameters.AddWithValue("@NOME", conta.Nome);
             comando.Parameters.AddWithValue("@VALOR", conta.Valor);
             comando.Parameters.AddWithValue("@TIPO", conta.Tipo);
             comando.Parameters.AddWithValue("@DESCRICAO", conta.Descricao);
-            comando.Parameters.AddWithValue("STATUS", conta.Status);
+            comando.Parameters.AddWithValue("@STATUS", conta.Status);
 
             int id = Convert.ToInt32(comando.ExecuteScalar());
             comando.Connection.Close();
@@ -37,11 +37,11 @@ namespace Repository
 
         public List<ContaPagar> ObterTodos(string busca)
         {
-            SqlCommand comando = new SqlCommand();
-            comando.CommandText = @"INSERT FROM * contas_pagar WHERE nome LIKE @NOME";
+            SqlCommand comando = conexao.Conectar();
+            comando.CommandText = @"SELECT * FROM contas_pagar WHERE nome LIKE @NOME";
 
             busca = $"%{busca}%";
-            comando.Parameters.AddWithValue("NOME", busca);
+            comando.Parameters.AddWithValue("@NOME", busca);
 
             DataTable tabela = new DataTable();
             tabela.Load(comando.ExecuteReader());
@@ -66,7 +66,7 @@ namespace Repository
 
         public ContaPagar ObterPeloId (int id)
         {
-            SqlCommand comando = new SqlCommand();
+            SqlCommand comando = conexao.Conectar();
             comando.CommandText = @"SELECT * FROM contas_pagar WHERE id = @ID";
             comando.Parameters.AddWithValue("ID", id);
             DataTable tabela = new DataTable();
@@ -91,7 +91,7 @@ namespace Repository
 
         public bool Atualizar (ContaPagar conta)
         {
-            SqlCommand comando = new SqlCommand();
+            SqlCommand comando = conexao.Conectar();
             comando.CommandText = @"UPDATE contas_pagar SET nome = @NOME, valor = @VALOR, tipo = @TIPO, descricao = @DESCRICAO, status = @STATUS WHERE id = @ID";
 
             comando.Parameters.AddWithValue("@ID", conta.Id);
